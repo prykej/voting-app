7c2459f17a1308f648e07a93d39638d1e0d93921
Jim Pryke <jim.pryke@netinstitute.com>
2017-08-01 18:22:01 -0500

    Refactor with transform-class-properties

--- public/js/app.js
+++ public/js/app.js
@@ -1,20 +1,13 @@
 class ProductList extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      products: [],
-    };
-
-    this.handleProductUpVote = this.handleProductUpVote.bind(this);
-  }
+  state = {
+    products: [],
+  };
 
   componentDidMount() {
     this.setState({ products: Seed.products });
   }
 
-  // Inside `ProductList`
-  handleProductUpVote(productId) {
+  handleProductUpVote = (productId) => {
     const nextProducts = this.state.products.map((product) => {
       if (product.id === productId) {
         return Object.assign({}, product, {
@@ -55,15 +48,9 @@ class ProductList extends React.Component {
 }
 
 class Product extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.handleUpVote = this.handleUpVote.bind(this);
-  }
-
-  handleUpVote() {
-    this.props.onVote(this.props.id);
-  }
+  handleUpVote = () => (
+    this.props.onVote(this.props.id)
+  );
 
   render() {
     return (
